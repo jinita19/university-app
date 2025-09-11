88dd2716ffa29edc87caf9502c20085e8d77db9b
Jini's Macbook <jinismacbook@Jiniss-MacBook-Air.local>
2025-09-11 15:48:02 -0700

    Created basic setup for frontend

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import '../src/styles/global.css'
+
+function App() {
+  return (
+    <div className='app-container'>       
+      <h1>University App</h1>
+    </div>
+  )
+}
+
+export default App
